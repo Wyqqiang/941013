78ffa12e3d974cd994992fa6ecf2b9df9505eb99
Wyaqiang <852952709@qq.com>
2020-07-10 15:37:07 +0800

    main.java

--- src/com/isoftstone/Main.java
+++ src/com/isoftstone/Main.java
@@ -3,7 +3,8 @@ package com.isoftstone;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("demogit");
+       // System.out.println("demogit");
+        System.out.println(123);
         System.out.println("add message");
     }
 }
