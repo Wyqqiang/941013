017c1f2557954b63f3f9c666121c9752bdfe409f
Wyaqiang <852952709@qq.com>
2020-07-13 10:00:42 +0800

    b

--- src/com/isoftstone/Main.java
+++ src/com/isoftstone/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 
 
-        //System.out.println("add message");
+        System.out.println("add message");
         System.out.println("add message");
         System.out.println("add message");
         System.out.println("add message");
