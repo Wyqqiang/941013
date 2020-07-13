f949f8b7521985444f382f5de38ab0d55a55e7de
Wyaqiang <852952709@qq.com>
2020-07-13 10:32:02 +0800

    测试本地提交到远程的冲突

--- src/com/isoftstone/Main.java
+++ src/com/isoftstone/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
 
         System.out.println("add temp_test1212");
-        System.out.println("add message");
-        System.out.println("add message");
+        System.out.println("add message1212");
+        //System.out.println("add message");
         System.out.println("add message");
         System.out.println("add message");
     }
